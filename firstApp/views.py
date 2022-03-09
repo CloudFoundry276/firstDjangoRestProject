f7846132a69d93542cfa39148a0028389ea1a41f
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-10 00:12:27 +0530

    django rest framework basic project with mysql dd

---
+++ firstApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from firstApp.models import Employee
+
+
+# Create your views here.
+def employeeView(request):
+    emp = {
+        'id': 123,
+        'name': 'John',
+        'salary': 50000
+    }
+
+    data = Employee.objects.all()
+    response = {'employees': list(data.values('name', 'salary'))}
+
+    return JsonResponse(response)
